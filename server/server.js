1382f96fd907030d9cc7ff813e58a79231a8ede3
Conor Sexton <sextonc@me.com>
2019-05-14 21:18:06 -0400

    Add basic Markdown parsing with marked.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const md = require('marked');
 
 const app = express();
 const PORT = 3000;
@@ -8,8 +9,8 @@ const PORT = 3000;
 app.use(bodyParser.text())
 
 app.post('/api/parse', (req, res, next) => {
-  console.log(req.body);
-  res.json(req.body);
+  res.locals.text = md(req.body);
+  next();
 });
 
 app.listen(3000);
