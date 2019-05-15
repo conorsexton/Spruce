c7a86988cfa6f6307476f661e1ce451dfb1a5673
Conor Sexton <sextonc@me.com>
2019-05-15 11:23:12 -0400

    Added basic rendering and reset functionality

--- client/main.jsx
+++ client/main.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Form from '../containers/Form.jsx';
+import Reset from '../components/Reset.jsx';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { mode: 'input' };
     this.getResults = this.getResults.bind(this);
+    this.resetMode = this.resetMode.bind(this);
   }
 
   getResults(html) {
@@ -16,11 +18,24 @@ class App extends Component {
     });
   }
 
+  resetMode() {
+    this.setState({
+      mode: 'input',
+    })
+  }
+
   render() {
+    if (this.state.mode === 'input') {
+      return (
+        <section>
+          {this.state.mode === 'input' && <Form handleResults={this.getResults} />}
+        </section>
+      )
+    }
     return (
       <section>
-        {this.state.mode === 'input' && <Form handleResults={this.getResults} />}
-        {this.state.mode === 'results' && this.state.results}
+      {this.state.results}
+      <Reset handleClick={this.resetMode} />
       </section>
     );
   }
---
+++ components/Reset.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Reset = props => {
+  return (
+    <button onClick={props.handleClick}>Reset</button>
+  )
+}
+
+export default Reset;
